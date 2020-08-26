54fb7e7c42f76155e8d81ca1f260786da23b08af
Raman Lebedinsky <romkaleb@gmail.com>
2020-08-26 03:43:56 +0300

    Added example WaypointsRerouteActivity.

--- examples/src/main/java/com/mapbox/navigation/examples/CoreActivity.kt
+++ examples/src/main/java/com/mapbox/navigation/examples/CoreActivity.kt
@@ -27,6 +27,7 @@ import com.mapbox.navigation.examples.core.RouteAlertsActivity
 import com.mapbox.navigation.examples.core.RuntimeRouteStylingActivity
 import com.mapbox.navigation.examples.core.SimpleMapboxNavigationKt
 import com.mapbox.navigation.examples.core.SummaryBottomSheetActivity
+import com.mapbox.navigation.examples.core.WaypointsRerouteActivity
 import kotlinx.android.synthetic.main.activity_core.*
 
 class CoreActivity : AppCompatActivity() {
@@ -105,6 +106,11 @@ class CoreActivity : AppCompatActivity() {
                 getString(R.string.title_replay_waypoints),
                 getString(R.string.description_replay_waypoints),
                 ReplayWaypointsActivity::class.java
+            ),
+            SampleItem(
+                getString(R.string.title_waypoints_reroute),
+                getString(R.string.description_waypoints_reroute),
+                WaypointsRerouteActivity::class.java
             ),
             SampleItem(
                 getString(R.string.title_faster_route),
